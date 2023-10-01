6530711c57d2311c846074c3579a2daa43491185
chewjh1234 <111339807+chewjh1234@users.noreply.github.com>
2023-10-02 02:33:44 +0800

    Add SortCommand by Courses draft

---
+++ src/main/java/seedu/address/logic/commands/SortCourseCommand.java
@@ -0,0 +1,11 @@
+package seedu.address.logic.commands;
+
+public class SortCourseCommand {
+
+    public static final String COMMAND_WORD = "sort course";
+
+    public static final String MESSAGE_SUCCESS = "Sorted all persons by course";
+
+    
+
+}
